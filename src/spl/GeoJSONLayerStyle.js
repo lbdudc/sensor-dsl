c07f11deee41b99c404f26e78eee49fcc1599b6b
victorlamas <victor.lamas@udc.es>
2024-04-12 12:01:02 +0200

    added radius default style fot geojson style layers

--- src/spl/GeoJSONLayerStyle.js
+++ src/spl/GeoJSONLayerStyle.js
@@ -1,11 +1,12 @@
 export default class GeoJSONLayerStyle {
-  constructor(id, fillColor, strokeColor, fillOpacity, strokeOpacity) {
+  constructor(id, fillColor, strokeColor, fillOpacity, strokeOpacity, radius) {
     this.name = id;
     this.type = "GeoJSONLayerStyle";
     this.fillColor = fillColor;
     this.strokeColor = strokeColor;
     this.fillOpacity = fillOpacity;
     this.strokeOpacity = strokeOpacity;
+    this.radius = radius || 3;
   }
 
   getId() {
